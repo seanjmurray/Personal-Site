87d144e369c398fdd607c01c872e16cc1c7b7ae5
Sean Murray <sean.j.murray1@gmail.com>
2020-05-03 17:13:49 -0700

    Reworking html, removing bootstrap

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+// Scroll bar
+window.onscroll = () => {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById('bar').style.width = scrolled + '%'};
+
+
 
 var app = document.getElementById('typeWrite');
 
